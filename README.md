d8da8783126e3a26cdf95c12fec4320b0f5f0e44
XmchxUp <1394466835@qq.com>
2022-05-24 15:36:36 +0800

    finish lab0

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cs61c-sp22
+
+
+
+### TODO
+- [X] Lab 0: Intro and Setup
--- sp22-lab-starter/lab00/code.py
+++ sp22-lab-starter/lab00/code.py
@@ -5,9 +5,11 @@ def get_airspeed_velocity_of(unladen_swallow):
     return # redacted
 
 def fizzbuzz(num):
-  if num == 3: # edit this line
+  if num % 15 == 0:
+    print(f"{num}: fizzbuzz")
+  elif num % 3 == 0: # edit this line
     print(f"{num}: fizz")
-  if num == 5: # edit this line
+  elif num % 5 == 0: # edit this line
     print(f"{num}: buzz")
 
 for i in range(1, 20):
