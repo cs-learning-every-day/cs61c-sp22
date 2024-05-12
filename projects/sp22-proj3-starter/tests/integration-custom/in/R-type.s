7a5e256a5aac953d80d7fa6b92813e0fa01f0784
Ultraman <1394466835@qq.com>
2024-05-12 08:49:03 +0800

    feat: finish proj3 task6

---
+++ projects/sp22-proj3-starter/tests/integration-custom/in/R-type.s
@@ -0,0 +1,13 @@
+add t0, x0, t1
+sub t0, x0, t1
+and t0, x0, t1
+or t0, x0, t1
+xor t0, x0, t1
+sll t0, x0, t1
+srl t0, x0, t1
+sra t0, x0, t1
+slt t0, x0, t1
+mul t0, x0, t1
+mulh t0, x0, t1
+mulhu t0, x0, t1
+
